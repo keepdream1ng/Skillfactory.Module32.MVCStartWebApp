f668acc759af4d080fa3532474a6da969815df56
Anatolii Ivanov <gotnickname@gmail.com>
2023-10-07 08:40:17 +0530

    Add project files.

---
+++ Skillfactory.Module32.MVCStartWebApp/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿namespace Skillfactory.Module32.ASPCoreMVC.Middleware;
+public class LoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public LoggingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    private async Task LogContext(HttpContext context)
+    {
+        string logMessage = $"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}{Environment.NewLine}";
+        Console.WriteLine(logMessage);
+
+        string logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "RequestLog.txt");
+        await File.AppendAllTextAsync(logFilePath, logMessage);
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        // Do we really need to wait till logger will finish?
+        LogContext(context);
+        await _next.Invoke(context);
+    }
+}
---
+++ Skillfactory.Module32.MVCStartWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Skillfactory.Module32.MVCStartWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
